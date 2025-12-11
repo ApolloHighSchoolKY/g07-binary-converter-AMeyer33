6a8ba7fd8dfee69e9976e0b4fbc1d0936e802262
AMeyer33 <andrew.meyer@stu.daviess.kyschools.us>
2025-12-11 20:22:56 +0000

    Commit

--- BinaryConverter.java
+++ BinaryConverter.java
@@ -30,24 +30,60 @@ public class BinaryConverter
     	return value;
     }
 
+    public String getBinary()
+    {
+    	return binaryValue;
+    }
+
     public void setValue(int newValue)
     {
     	value=newValue;
     }
 
+    public void setBinary(String binary)
+    {
+    	binaryValue = binary;
+    }
+
     public String toBinary()
     {
     	//Convert the integer value to binary
-
-
-    	return "";
+        int valuetemporary = value;
+        String binaryoutput = "";
+        for(int i = 7; i > 0; i--)
+            if(valuetemporary >= Math.pow(2, i))
+            {
+                valuetemporary -= Math.pow(2, i);
+                binaryoutput += "1";
+            }
+            else
+            {
+                binaryoutput += "0";
+            }
+            
+    	return "" + binaryoutput;
     }
 
     public int toDecimal()
     {
     	//Convert the binary value to decimal
+        int valuetemp = 0;
+        for(int i = 0; i < 8; i++)
+            if(binaryValue.charAt(i) == '1')
+            {
+                valuetemp += Math.pow(2,7-i);
+            }
 
-    	return 0;
+    	return valuetemp;
+    }
+
+    public String toString()
+    {
+        //return the information needed when the converter runs
+        return "" +"\n" + "The entered integer was " + value + "\n"
+                        + "The integer converted to binary is " + toBinary() + "\n"
+                        + "The entered binary number was " + binaryValue + "\n"
+                        + "The binary number converted to decimal is " + toDecimal() + "\n";
     }
 
 
--- BinaryConverterRunner.java
+++ BinaryConverterRunner.java
@@ -8,7 +8,7 @@ public class BinaryConverterRunner
 
     	System.out.println(steve.getValue());
 
-    	steve.setValue(100);
+    	steve.setValue(152);
 
     	System.out.println(steve.getValue());
 
@@ -16,11 +16,15 @@ public class BinaryConverterRunner
 
     	System.out.println((int)Math.pow(2,4));
 
-    	BinaryConverter jane = new BinaryConverter("11010011");
+    	BinaryConverter jane = new BinaryConverter("10011001");
+
+		jane.setValue(152);
 
     	System.out.println(jane.getBinary());
 
     	System.out.println(jane.toDecimal());
+
+		System.out.println(jane);
     }
 
 
